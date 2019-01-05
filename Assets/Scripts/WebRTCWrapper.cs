b0187817f1becd936f1f92cef077ffdc3f9ae81e
ein-bandit <kaufi07@gmail.com>
2019-01-05 14:49:48 +0100

    inital commit with unity sources and git ignore.

---
+++ Assets/Scripts/WebRTCWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebRTCWrapper : MonoBehaviour
+{
+    public Text data;
+    public GameObject cube;
+
+    public bool startServer = true;
+
+    private string text = "";
+
+    private WebRTCServer wss;
+
+    void Awake()
+    {
+        if (startServer)
+        {
+            wss = new WebRTCServer(1234);
+            wss.OnCallbackDataMessage += OnReceiveData;
+        }
+
+    }
+
+    public void OnReceiveData(string msg)
+    {
+        UnityEngine.Debug.Log($"received data ${msg}");
+        text = msg;
+    }
+
+    void Update()
+    {
+        data.text = text;
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("quitting");
+        wss.Dispose();
+    }
+}
