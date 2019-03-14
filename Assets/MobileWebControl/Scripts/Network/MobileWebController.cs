84694daca869ed4612b69c97985c79be0d38b630
ein-bandit <kaufi07@gmail.com>
2019-03-14 20:10:56 +0100

    removed unused litjson imports.

--- Assets/MobileWebControl/Scripts/Network/MobileWebController.cs
+++ Assets/MobileWebControl/Scripts/Network/MobileWebController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using LitJson;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
--- Assets/MobileWebControl/Scripts/Network/WebRTC/IWebRTCServer.cs
+++ Assets/MobileWebControl/Scripts/Network/WebRTC/IWebRTCServer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using LitJson;
 
 namespace MobileWebControl.Network.WebRTC
 {
